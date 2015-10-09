563efa6e87e15d502261574697d11c00d1224485
Vitaly-V <gtopbox@gmail.com>
2015-10-09 08:52:50 +0000

    Simple github card component

--- script.jsx
+++ script.jsx
@@ -1,37 +1,55 @@
-var Button = React.createClass({
-    localHandleClick: function() {
-        this.props.localHandleClick(this.props.increment);
+var Card = React.createClass({
+    getInitialState: function () {
+        return {};
     },
-    render: function() {
-        return (
-            <button onClick={this.localHandleClick}>{this.props.increment}</button>
-        )
-    }
-});
-
-var Result = React.createClass({
-    render: function() {
-        return (
-            <div>{this.props.localCounter}</div>
-        )
-    }
-});
-
-var Main = React.createClass({
-    getInitialState: function() {
-        return {counter: 0};
+    componentDidMount: function () {
+        var component = this;
+        $.get("https://api.github.com/users/" + this.props.login, function (data) {
+            component.setState(data)
+        });
     },
-    handleClick: function(increment) {
-        this.setState({ counter: this.state.counter+increment });
-    },
-    render: function() {
+    render: function () {
         return (
             <div>
-                <Button localHandleClick={this.handleClick} increment={1} />
-                <Button localHandleClick={this.handleClick} increment={5} />
-                <Button localHandleClick={this.handleClick} increment={10} />
-                <Button localHandleClick={this.handleClick} increment={100} />
-                <Result localCounter={this.state.counter} />
+                <img src={this.state.avatar_url} width="80"/>
+
+                <h3>{this.state.name}</h3>
+                <hr/>
+            </div>
+        )
+    }
+});
+var Form = React.createClass({
+    handleSubmit: function(e) {
+      e.preventDefault();
+      var loginInput = ReactDOM.findDOMNode(this.refs.login);
+        this.props.addCard(loginInput.value);
+        loginInput.value = '';
+    },
+    render: function () {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <input type="text" placeholder="github login" ref="login" />
+                <button>Add</button>
+            </form>
+        );
+    }
+});
+var Main = React.createClass({
+    getInitialState: function () {
+        return {logins: []}
+    },
+    addCard: function(loginToAdd) {
+      this.setState({logins: this.state.logins.concat(loginToAdd)});
+    },
+    render: function () {
+        var cards = this.state.logins.map(function (login) {
+            return (<Card login={login}/>);
+        });
+        return (
+            <div>
+                <Form addCard={this.addCard} />
+                {cards}
             </div>
         )
     }
